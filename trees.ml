539a40e6b48f3607a5aab98b50f7d465758534aa
Erick Matsen <ematsen@gmail.com>
2009-10-24 15:32:40 -0700

    changed things to use the parsing in btree_io 	modified:   trees.ml

--- trees.ml
+++ trees.ml
@@ -3,9 +3,9 @@ let all_trees =
   List.map 
     (fun (tree_str, tree_name) ->
       (Btree_io.of_newick_str tree_str, tree_name))
-[
-"((0,1),(2,3))",          "p_reduced4";
-"((0,1),((2,3),4))",      "p_reduced5";
-"(((0,1),4),((2,3),5))",  "p_reduced6a";
-"(((0,1),(2,3)),(4,5))",  "p_reduced6b";
-]
+  [
+  "((0,1),(2,3))",          "p_reduced4";
+  "((0,1),((2,3),4))",      "p_reduced5";
+  "(((0,1),4),((2,3),5))",  "p_reduced6a";
+  "(((0,1),(2,3)),(4,5))",  "p_reduced6b";
+  ]
