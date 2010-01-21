3a579611fcf1bc3d21ff0e775dd76b47da5eb3b8
Erick Matsen <ematsen@gmail.com>
2010-01-21 09:59:50 -0800

    making p_reduced7

--- trees.ml
+++ trees.ml
@@ -4,8 +4,10 @@ let all_trees =
     (fun (tree_str, tree_name) ->
       (Btree_io.of_newick_str tree_str, tree_name))
   [
-  "((0,1),(2,3))",          "p_reduced4";
-  "((0,1),((2,3),4))",      "p_reduced5";
-  "(((0,1),4),((2,3),5))",  "p_reduced6a";
-  "(((0,1),(2,3)),(4,5))",  "p_reduced6b";
+"((0,1),(2,3))",              "p_reduced4";
+"((0,1),((2,3),4))",          "p_reduced5";
+"(((0,1),4),((2,3),5))",      "p_reduced6a";
+"(((0,1),(2,3)),(4,5))",      "p_reduced6b";
+"(((0,1),4),(((2,3),6),5))",  "p_reduced7a";
+"(((0,1),(2,3)),((4,5),6))",  "p_reduced7b";
   ]
